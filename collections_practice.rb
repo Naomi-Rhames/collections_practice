bf1a475d2d281fb4471dae51dd4990a2a9f52f9c
Naomi-Rhames <naomirhames2020@gmail.com>
2020-11-17 13:44:37 -0600

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
-
+require "pry"
+def sort_array_asc(integers)
+    integers.sort! do |x,y|
+        x <=> y
+    end
+end
+def sort_array_desc(integers)
+    integers.sort! do |x,y|
+        y <=> x 
+    end
+end
+def sort_array_char_count(array)
+    array.sort do |x,y|
+        x.length <=> y.length
+      
+    end
+end
+def swap_elements(array)
+    array.sort do |x,y|
+        x.length <=> y.length
+    end
+end
+def reverse_array(integers)
+    integers.reverse do |x,y|
+        y <=> x
+    end
+end
+def kesha_maker(input)   
+   new_arr = []
+    input.map do |element| 
+        element[2] = "$"
+        new_arr.push(element)
+    end
+    new_arr
+end
+def find_a(array)
+    array.select do |element| 
+        element.start_with?("a")
+    end
+end
+def sum_array(integers)
+    integers.inject{|sum,number| sum + number}
+end
+def add_s(array)
+    body = []
+    array.each do |element| 
+    if element == "feet"
+        body.push(element)
+    else
+        body.push "#{element}s"
+         end
+    end
+    body
+end
